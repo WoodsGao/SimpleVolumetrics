b3a875565515dd32f182c7cbe846477cc60dcb2d
woods <woodsgao@qq.com>
2023-01-31 18:26:25 +0800

    fix light decay

--- Assets/SimpleVolumetrics/Shaders/Volumetric.hlsl
+++ Assets/SimpleVolumetrics/Shaders/Volumetric.hlsl
@@ -93,7 +93,7 @@ float4 frag(Varyings input) : SV_Target
 #if VOLUMETRIC_TYPE_FOG
     float intensity = _Intensity;
 #else
-    float intensity = exp2(-rayLength * rayLength / _Intensity);
+    float intensity = _Intensity / (rayLength * rayLength);
 #endif
 
 #if VOLUMETRIC_SHADOW_ON
@@ -103,7 +103,7 @@ float4 frag(Varyings input) : SV_Target
     float distance = max(0, depth - rayLength);
     // return half4(distance, -distance, 0, 1);
 
-    intensity *= lerp(_ShadowSoftness, 1, 1 - exp2(- distance * distance * _EdgeContrast));
+    intensity *= lerp(_ShadowSoftness, 1, 1 - exp(- distance * _EdgeContrast));
 #endif
 
     float noise = GetNoise3D(input.noiseCoord.xyz);
